065d267bf381d6456db987e92ca545ed63f21c06
Carlos del Hoyo Muñoz <cdelhoyo@autentia.com>
2018-10-05 12:17:10 +0200

    CrossOrigin activated

--- src/main/java/com/cdelhoyo/cursosboot/web/CourseController.java
+++ src/main/java/com/cdelhoyo/cursosboot/web/CourseController.java
@@ -6,11 +6,11 @@ import com.cdelhoyo.cursosboot.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/courses")
+@CrossOrigin
 public class CourseController {
 
 	private CourseService courseService;
--- src/main/java/com/cdelhoyo/cursosboot/web/TeacherController.java
+++ src/main/java/com/cdelhoyo/cursosboot/web/TeacherController.java
@@ -6,11 +6,11 @@ import com.cdelhoyo.cursosboot.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/teachers")
+@CrossOrigin
 public class TeacherController {
 
 	private TeacherService teacherService;
