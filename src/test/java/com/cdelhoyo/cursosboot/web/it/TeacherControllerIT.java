9c0bcbbada566aae8c1fb57a2eaf7a58bf8a63ae
Carlos del Hoyo Muñoz <cdelhoyo@autentia.com>
2018-10-05 15:22:27 +0200

    Configure IT

--- src/test/java/com/cdelhoyo/cursosboot/web/it/TeacherControllerIT.java
+++ src/test/java/com/cdelhoyo/cursosboot/web/it/TeacherControllerIT.java
@@ -11,9 +11,12 @@ import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +25,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+@Rollback
 public class TeacherControllerIT {
 
     @Autowired
